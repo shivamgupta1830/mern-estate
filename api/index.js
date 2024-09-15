af3cdfddc5115742fc2ac1d3ee150b58b2914d37
Shivam Gupta <shivamgupta1830@gmail.com>
2024-09-15 10:06:06 +0530

    DB setup

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+
+import dotenv from "dotenv";
+import { error } from "console";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB connected"))
+  .catch((error) => console.log(error));
 
 const app = express();
 
