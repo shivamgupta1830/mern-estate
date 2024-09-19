e8ed0476bf09c2974f9d39e75fd7edd920f29122
Shivam Gupta <shivamgupta1830@gmail.com>
2024-09-19 22:58:01 +0530

    Complete listing page UI

--- client/src/pages/ListingPage.jsx
+++ client/src/pages/ListingPage.jsx
@@ -5,6 +5,15 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
 import { Navigation } from "swiper/modules";
 import "swiper/css/bundle";
+import {
+  FaBath,
+  FaBed,
+  FaChair,
+  FaMapMarkerAlt,
+  FaParking,
+  FaShare,
+} from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 function ListingPage() {
   SwiperCore.use([Navigation]);
@@ -12,6 +21,10 @@ function ListingPage() {
   const [listingData, setListingData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  const [copied, setCopied] = useState(false);
+  const [contact, setContact] = useState(false);
+
+  const { currentUser } = useSelector((state) => state.user);
 
   const params = useParams();
   const listingID = params.listingID;
@@ -31,7 +44,6 @@ function ListingPage() {
 
         setLoading(false);
         setListingData(data);
-        console.log(listingData);
       } catch (error) {
         setError(true);
         setLoading(false);
@@ -43,30 +55,17 @@ function ListingPage() {
 
   return (
     <main className="min-h-screen">
-      {loading && (
-        <p className="text-center my-9 font-bold text-3xl">Loading...</p>
-      )}
+      {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
       {error && (
-        <div className="flex flex-col gap-8 justify-center items-center my-10">
-          <p className="text-center  font-bold text-3xl">
-            Something went wrong !!
-          </p>
-          <Link to="/">
-            {" "}
-            <button className="text-white bg-slate-700 hover:bg-slate-800 p-3 rounded-lg">
-              Back to Home
-            </button>
-          </Link>
-        </div>
+        <p className="text-center my-7 text-2xl">Something went wrong!</p>
       )}
-
-      {listingData && !error && !loading && (
-        <>
+      {listingData && !loading && !error && (
+        <div>
           <Swiper navigation>
             {listingData.imageUrls.map((url) => (
               <SwiperSlide key={url}>
                 <div
-                  className="h-[500px]"
+                  className="h-[450px]"
                   style={{
                     background: `url(${url}) center no-repeat`,
                     backgroundSize: "cover",
@@ -75,7 +74,86 @@ function ListingPage() {
               </SwiperSlide>
             ))}
           </Swiper>
-        </>
+          <div className="fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer">
+            <FaShare
+              className="text-slate-500"
+              onClick={() => {
+                navigator.clipboard.writeText(window.location.href);
+                setCopied(true);
+
+                setTimeout(() => {
+                  setCopied(false);
+                }, 2000);
+              }}
+            />
+          </div>
+          {copied && (
+            <p className="fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2">
+              Link copied!
+            </p>
+          )}
+          <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4 px-8 md:px-1 ">
+            <p className="text-2xl font-semibold">
+              {listingData.name} - $
+              {listingData.offer
+                ? listingData.discountedPrice.toLocaleString("en-US")
+                : listingData.regularPrice.toLocaleString("en-US")}
+              {listingData.type === "rent" && "/month"}
+            </p>
+            <p className="flex items-center mt-6 gap-2 text-slate-600  text-sm">
+              <FaMapMarkerAlt className="text-green-700" />
+              {listingData.address}
+            </p>
+            <div className="flex gap-4">
+              <p className="bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                {listingData.type === "rent" ? "For Rent" : "For Sale"}
+              </p>
+              {listingData.offer && (
+                <p className="bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
+                  ${+listingData.regularPrice - +listingData.discountedPrice}{" "}
+                  OFF
+                </p>
+              )}
+            </div>
+            <div className="text-slate-800 flex flex-col gap-2">
+              <span className="font-semibold text-black">Description</span>
+              <p>{listingData.description}</p>
+            </div>
+            <ul className="text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6">
+              <li className="flex items-center gap-1 whitespace-nowrap ">
+                <FaBed className="text-lg" />
+                {listingData.bedrooms > 1
+                  ? `${listingData.bedrooms} Bedrooms `
+                  : `${listingData.bedrooms} Bedroom `}
+              </li>
+              <li className="flex items-center gap-1 whitespace-nowrap ">
+                <FaBath className="text-lg" />
+                {listingData.bathrooms > 1
+                  ? `${listingData.bathrooms} Bathrooms `
+                  : `${listingData.bathrooms} Bathroom `}
+              </li>
+              <li className="flex items-center gap-1 whitespace-nowrap ">
+                <FaParking className="text-lg" />
+                {listingData.parking ? "Parking spot" : "No Parking"}
+              </li>
+              <li className="flex items-center gap-1 whitespace-nowrap ">
+                <FaChair className="text-lg" />
+                {listingData.furnished ? "Furnished" : "Unfurnished"}
+              </li>
+            </ul>
+            {currentUser &&
+              listingData.userRef !== currentUser._id &&
+              !contact && (
+                <button
+                  onClick={() => setContact(true)}
+                  className="bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3"
+                >
+                  Contact Owner
+                </button>
+              )}
+            {/* {contact && <Contact listing={listing} />} */}
+          </div>
+        </div>
       )}
     </main>
   );
