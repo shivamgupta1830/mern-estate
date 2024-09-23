42094c096a175582a4b1154ae6a3406140ffcd55
Shivam Gupta <shivamgupta1830@gmail.com>
2024-09-23 12:01:34 +0530

    completed Abou page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 
 function About() {
-  return <div className="min-h-screen">About</div>;
+  return (
+    <div className="min-h-screen flex flex-col justify-start items-start py-12 px-6 lg:px-[6.5rem] gap-4 text-gray-800">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Shivam Estate
+      </h1>
+      <p>
+        Welcome to <span className="font-bold">Shivam Estate</span>, your
+        trusted partner in the world of real estate. Whether you’re searching
+        for your dream home, a luxury villa, or a modern apartment, Shivam
+        Estate is committed to helping you find the{" "}
+        <span className="font-bold">perfect property</span> that suits your
+        lifestyle and needs.
+      </p>
+      <p>
+        With years of experience in the real estate industry, we specialize in
+        providing seamless solutions for both{" "}
+        <span className="font-bold">rentals and sales</span>. Our diverse
+        portfolio features homes, villas, and apartments in prime locations,
+        catering to various tastes and budgets. We understand that finding the
+        right home is a monumental decision, and we are here to make the process
+        as smooth and rewarding as possible.
+      </p>
+      <p>
+        At Shivam Estate, we believe in building lasting relationships with our
+        clients through
+        <span className="font-bold">
+          {" "}
+          honesty, integrity, and exceptional service
+        </span>
+        . Our team of dedicated professionals is passionate about helping you
+        navigate the ever-changing real estate market, offering personalized
+        advice and guidance every step of the way.
+      </p>
+      <p>
+        Whether you're a first-time buyer, a seasoned investor, or someone
+        looking for a temporary rental, Shivam Estate is your gateway to the
+        best properties. Explore our listings and let us help you turn your
+        <span className="font-bold"> real estate dreams into reality</span> .
+      </p>
+      <h3 className="font-bold">Shivam Estate – Your Home, Our Mission.</h3>
+    </div>
+  );
 }
 
 export default About;
